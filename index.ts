212691d577d1ddffc989689fa8960788e8a04c4f
gyandx <59648916+gyandx@users.noreply.github.com>
2022-11-16 20:53:21 +0530

    obserable name update

--- index.ts
+++ index.ts
@@ -2,15 +2,15 @@ import './style.css';
 
 import { of, Observable } from 'rxjs';
 
-const student: Observable<String> = of('Rahul');
-const studentList: Observable<String[]> = of([
+const student$: Observable<String> = of('Rahul');
+const studentList$: Observable<String[]> = of([
   'Ram',
   'Sita',
   'Laxman',
   'Hanuman',
 ]);
-const studentObj: Observable<any> = of({ name: 'Rohit', age: 22 });
+const studentObj$: Observable<any> = of({ name: 'Rohit', age: 22 });
 
-student.subscribe((res) => console.log(res));
-studentList.subscribe((res) => console.log(res));
-studentObj.subscribe((res) => console.log(res));
+student$.subscribe((res) => console.log(res));
+studentList$.subscribe((res) => console.log(res));
+studentObj$.subscribe((res) => console.log(res));
