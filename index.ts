3813aabf762d8de8839e7d503dc3a0b7c91409fc
gyandx <59648916+gyandx@users.noreply.github.com>
2022-11-16 17:25:50 +0530

    from operator

--- index.ts
+++ index.ts
@@ -1,23 +1,5 @@
-import './style.css';
+import { Observable, from } from 'rxjs';
 
-import { Observable } from 'rxjs';
+let students: Observable<string> = from(['Mohit', 'Rohit', 'Shayam']);
 
-let observer = new Observable((observer) => {
-  try {
-    observer.next('Ram');
-    // observer.next('Rohit');
-    // observer.next('Rahul');
-    setInterval(() => {
-      observer.next('Rohit');
-    }, 3000);
-    setInterval(() => {
-      observer.next('Rahul');
-    }, 6000);
-  } catch (e) {
-    observer.error(e);
-  }
-});
-
-observer.subscribe((data) => {
-  console.log(data);
-});
+students.subscribe((eachStudent) => console.log(eachStudent));
